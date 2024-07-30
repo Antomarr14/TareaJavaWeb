4baa9fbc2c97fd456548de4a863294995e56e919
Antomarr14 <diegozetino8@gmail.com>
2024-07-29 21:05:25 -0600

    primera tarea

---
+++ src/main/java/com/ESFE/Asistenciass/AsistenciassApplication.java
@@ -0,0 +1,13 @@
+package com.ESFE.Asistenciass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenciassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ESFE/Asistenciass/AsistenciassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ESFE.Asistenciass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
